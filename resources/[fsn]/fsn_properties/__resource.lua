1d8c0d90a9b27f636b9796d9017685c08cea70bc
James Lee Scott <me@jamesscott.pw>
2018-06-27 21:48:51 +0100

    Setup for properties script
    ass

---
+++ resources/[fsn]/fsn_properties/__resource.lua
@@ -0,0 +1,9 @@
+client_script 'entarable/properties.lua'
+client_script 'enterable/client.lua'
+
+client_script 'nonenterable/properties.lua'
+client_script 'nonenterable/client.lua'
+
+server_script 'enterable/properties.lua'
+server_script 'nonenterable/properties.lua'
+server_script 'server.lua'
