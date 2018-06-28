f6ab85a4d0e3a34f386dc0369cb3d524dca1587e
James Lee Scott <me@jamesscott.pw>
2018-06-28 04:58:09 +0100

    :fsn_properties: Added houses
    Apparently I couldn't sleep so at 4:57AM I sat adding houses and their doors to the script.
    
    ToDo: Add ownership / inventory / shared ownership / ? rent ?

--- resources/[fsn]/fsn_properties/enterable/client.lua
+++ resources/[fsn]/fsn_properties/enterable/client.lua
@@ -1,28 +1,47 @@
 local enterable_properties = {}
-RegisterNetEvent('fsn_properties:doors:init')
-AddEventHandler('fsn_properties:doors:init', function(tbl)
-  enterable_properties = tbl
-end)
-
 local char_id = 0
 AddEventHandler('fsn_main:character', function(char)
   char_id = char.char_id
   TriggerServerEvent('fsn_properties:doors:request')
 end)
+RegisterNetEvent('fsn_properties:doors:init')
+AddEventHandler('fsn_properties:doors:init', function(tbl)
+  enterable_properties = tbl
+end)
+
+RegisterNetEvent('fsn_properties:doors:init')
+AddEventHandler('fsn_properties:doors:init', function(tbl)
+  print('got')
+  enterable_properties = tbl
+  for k, v in pairs(enterable_properties) do
+    if v.blip ~= false then
+      local blip = AddBlipForCoord(v.blip.x, v.blip.y, v.blip.z)
+      SetBlipSprite(blip, 350)
+      SetBlipAsShortRange(blip, true)
+      BeginTextCommandSetBlipName("STRING")
+      AddTextComponentString("House")
+      EndTextCommandSetBlipName(blip)
+    end
+  end
+end)
 
 Citizen.CreateThread(function()
-  for k, v in pairs(enterable_properties) do
-    local blip = AddBlipForCoord(v.blip.x, v.blip.y, v.blip.z)
-    SetBlipSprite(blip, 350)
-    SetBlipAsShortRange(blip, true)
-    BeginTextCommandSetBlipName("STRING")
-    AddTextComponentString("House")
-    EndTextCommandSetBlipName(blip)
-  end
   while true do
     Citizen.Wait(0)
     for propertyk, property in pairs(enterable_properties) do
       if GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)).x, GetEntityCoords(GetPlayerPed(-1)).y, GetEntityCoords(GetPlayerPed(-1)).z, property.blip.x, property.blip.y, property.blip.z, true) < 80 then
+        ------------------------------------------ Management section
+        if GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)).x, GetEntityCoords(GetPlayerPed(-1)).y, GetEntityCoords(GetPlayerPed(-1)).z, property.blip.x, property.blip.y, property.blip.z, true) < 5 then
+          DrawMarker(1,property.blip.x,property.blip.y,property.blip.z-1,0,0,0,0,0,0,1.001,1.0001,0.4001,0,155,255,175,0,0,0,0)
+          if GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)).x, GetEntityCoords(GetPlayerPed(-1)).y, GetEntityCoords(GetPlayerPed(-1)).z, property.blip.x, property.blip.y, property.blip.z, true) < 1 then
+            SetTextComponentFormat("STRING")
+          	AddTextComponentString("Press ~INPUT_PICKUP~ to view the property")
+          	DisplayHelpTextFromStringLabel(0, 0, 1, -1)
+            if IsControlJustPressed(0,38) then
+
+  					end
+          end
+        end
         ------------------------------------------ Door stuff
         for _doork, _door in pairs(property.doors) do
           local door = GetClosestObjectOfType(_door.loc.x, _door.loc.y, _door.loc.z, 1.0, _door.hash, false, false, false)
--- resources/[fsn]/fsn_properties/enterable/properties.lua
+++ resources/[fsn]/fsn_properties/enterable/properties.lua
@@ -12,6 +12,79 @@ enterable_properties = {
       [5] = {desc='storage front door',locked=true,hash=933053701,loc={x=709.989,y=-960.667,z=30.545},text={x=709.989,y=-960.667,z=30.545}},
     },
   },
+  [2] = {
+    title = 'Micheals Mansion',
+    owner = 1,
+    blip = {x = -821.80621337891, y = 174.43176269531, z = 71.542022705078},
+    rob_spot = {x = -807.07531738281, y = 172.42413330078, z = 72.834754943848},
+    doors = {
+      [1] = {desc='front left door',locked=true,hash=159994461,loc={x=-816.716,y=179.097,z=72.827},text={x=-816.716,y=179.097,z=72.827}},
+      [2] = {desc='front right door',locked=true,hash=-1686014385,loc={x=-816.106,y=177.510,z=72.827},text={x=-816.106,y=177.510,z=72.827}},
+      [3] = {desc='garage inside',locked=true,hash=-1563640173,loc={x=-806.281,y=186.024,z=72.624},text={x=-806.281,y=186.024,z=72.624}},
+      [4] = {desc='rear right left',locked=true,hash=1245831483,loc={x=-794.505,y=178.012,z=73.040},text={x=-794.505,y=178.012,z=73.040}},
+      [5] = {desc='rear right right',locked=true,hash=-1454760130,loc={x=-796.565,y=177.221,z=73.040},text={x=-796.565,y=177.221,z=73.040}},
+      [6] = {desc='rear rear left',locked=true,hash=1245831483,loc={x=-794.185,y=182.568,z=73.040},text={x=-794.185,y=182.568,z=73.040}},
+      [7] = {desc='rear rear right',locked=true,hash=-1454760130,loc={x=-793.394,y=180.507,z=73.040},text={x=-793.394,y=180.507,z=73.040}},
+    },
+  },
+  [3] = {
+    title = 'Trevors Trailer',
+    owner = 1,
+    blip = {x = 1979.0842285156, y = 3818.0192871094, z = 32.537998199463},
+    rob_spot = {x = 1982.3674316406, y = 3053.3635253906, z = 47.215076446533},
+    doors = {
+      [1] = {desc='front door',locked=true,hash=132154435,loc={x=1972.768,y=3815.365,z=33.663},text={x=1972.768,y=3815.365,z=33.663}},
+    },
+  },
+  [4] = {
+    title = 'Yellowjack',
+    owner = 1,
+    blip = {x = 1989.6041259766, y = 3054.9001464844, z = 47.215103149414},
+    rob_spot = {x = 1969.1922607422, y = 3814.564453125, z = 33.428730010986},
+    doors = {
+      [1] = {desc='front door',locked=true,hash=-287662406,loc={x=1991.105,y=3053.105,z=47.365},text={x=1991.105,y=3053.105,z=47.365}},
+    },
+  },
+  [5] = {
+    title = 'Shitbag\'s Flat',
+    owner = 1,
+    blip = {x = -1148.9720458984, y = -1523.6121826172, z = 10.628059387207},
+    rob_spot = {x = -1155.1801757813, y = -1523.9296875, z = 10.632334709167},
+    doors = {
+      [1] = {desc='front door',locked=true,hash=-607040053,loc={x=-1149.708,y=-1521.087,z=10.782},text={x=-1149.708,y=-1521.087,z=10.782}},
+    },
+  },
+  [6] = {
+    title = 'Franklin\'s aunt house',
+    owner = 1,
+    blip = {x = -13.11473941803, y = -1442.2130126953, z = 31.100654602051},
+    rob_spot = {x = -18.438192367554, y = -1438.5637207031, z = 31.101545333862},
+    doors = {
+      [1] = {desc='front door',locked=true,hash=520341586,loc={x=-14.868,y=-1441.182,z=31.193},text={x=-14.868,y=-1441.182,z=31.193}},
+    },
+  },
+  [7] = {
+    title = 'Stripclub',
+    owner = 1,
+    blip = {x = 127.66522216797, y = -1300.0837402344, z = 29.232746124268},
+    rob_spot = {x = -18.438192367554, y = -1438.5637207031, z = 31.101545333862},
+    doors = {
+      [1] = {desc='front door',locked=false,hash=-1116041313,loc={x=127.955,y=-1298.503,z=29.419},text={x=127.955,y=-1298.503,z=29.419}},
+      [2] = {desc='changing rooms',locked=true,hash=-495720963,loc={x=113.982,y=-1297.430,z=29.418},text={x=113.982,y=-1297.430,z=29.418}},
+      [3] = {desc='office inner',locked=true,hash=-626684119,loc={x=99.083,y=-1293.700,z=29.418},text={x=99.083,y=-1293.700,z=29.418}},
+      [4] = {desc='office outer',locked=true,hash=668467214,loc={x=96.091,y=-1284.853,z=29.438},text={x=96.091,y=-1284.853,z=29.438}},
+    },
+  },
+  [8] = {
+    title = 'Devins Garage',
+    owner = 1,
+    blip = {x = 484.38977050781, y = -1310.3785400391, z = 29.229318618774},
+    rob_spot = {x = 472.23861694336, y = -1310.4545898438, z = 29.222127914429},
+    doors = {
+      [1] = {desc='front door',locked=true,hash=-664582244,loc={x=482.811,y=-1311.953,z=29.350},text={x=482.811,y=-1311.953,z=29.350}},
+      [2] = {desc='garage',locked=true,hash=-190780785,loc={x=484.574,y=-1315.568,z=30.250},text={x=484.574,y=-1315.568,z=30.250}},
+    },
+  },
 }
 
 RegisterServerEvent('fsn_properties:doors:request')
