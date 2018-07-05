2e76a87dda5c47005408a02f171ab27a706feeef
James Lee Scott <me@jamesscott.pw>
2018-07-05 23:48:17 +0100

    :fsn_gui: test

--- resources/[fsn]/fsn_hungerandthirst/client.lua
+++ resources/[fsn]/fsn_hungerandthirst/client.lua
@@ -5,7 +5,7 @@ local UI = {
 	x =  0.000 ,	-- Base Screen Coords 	+ 	 x
 	y = -0.001 ,	-- Base Screen Coords 	+ 	-y
 }
-local init = false
+local init = true
 
 function drawRct(x,y,width,height,r,g,b,a)
 	DrawRect(x + width/2, y + height/2, width, height, r, g, b, a)
@@ -21,18 +21,33 @@ Citizen.CreateThread(function()
   while true do
     Citizen.Wait(1)
     if init then
-      drawRct(UI.x + 0.0149, UI.y + 0.797, 0.1569,0.0140,0,0,0,130) -- bar at top
-      drawRct(UI.x + 0.0149, UI.y + 0.800, 0.0771,0.0080,255,255,255,60) -- sub-bar (hunger)
-      drawRct(UI.x + 0.0941, UI.y + 0.800, 0.0771,0.0080,255,255,255,60) -- sub-bar (thirst)
+			if IsPedInAnyVehicle(GetPlayerPed(-1)) then
+	      drawRct(UI.x + 0.0149, UI.y + 0.797, 0.1569,0.0140,0,0,0,130) -- bar at top
+	      drawRct(UI.x + 0.0149, UI.y + 0.800, 0.0771,0.0080,255,255,255,60) -- sub-bar (hunger)
+	      drawRct(UI.x + 0.0941, UI.y + 0.800, 0.0771,0.0080,255,255,255,60) -- sub-bar (thirst)
 
 
-      local hunger_bar = 0.07715 / 100
-      hunger_bar = hunger_bar * hunger
+	      local hunger_bar = 0.07715 / 100
+	      hunger_bar = hunger_bar * hunger
 
-      local thirst_bar = 0.07715 / 100
-      thirst_bar = thirst_bar * thirst
-      drawRct(UI.x + 0.0149, UI.y + 0.800, hunger_bar,0.0080,17, 160, 41,130) -- main-bar (hunger)
-      drawRct(UI.x + 0.0942, UI.y + 0.800, thirst_bar,0.0080,0, 250, 255,130) -- main-bar (thirst)
+	      local thirst_bar = 0.07715 / 100
+	      thirst_bar = thirst_bar * thirst
+	      drawRct(UI.x + 0.0149, UI.y + 0.800, hunger_bar,0.0080,17, 160, 41,130) -- main-bar (hunger)
+	      drawRct(UI.x + 0.0942, UI.y + 0.800, thirst_bar,0.0080,0, 250, 255,130) -- main-bar (thirst)
+			else
+				drawRct(UI.x + 0.0149, UI.y + 0.697, 0.1569,0.0140,0,0,0,130) -- bar at top
+	      drawRct(UI.x + 0.0149, UI.y + 0.700, 0.0771,0.0080,255,255,255,60) -- sub-bar (hunger)
+	      drawRct(UI.x + 0.0941, UI.y + 0.700, 0.0771,0.0080,255,255,255,60) -- sub-bar (thirst)
+
+
+	      local hunger_bar = 0.07715 / 100
+	      hunger_bar = hunger_bar * hunger
+
+	      local thirst_bar = 0.07715 / 100
+	      thirst_bar = thirst_bar * thirst
+	      drawRct(UI.x + 0.0149, UI.y + 0.700, hunger_bar,0.0080,17, 160, 41,130) -- main-bar (hunger)
+	      drawRct(UI.x + 0.0942, UI.y + 0.700, thirst_bar,0.0080,0, 250, 255,130) -- main-bar (thirst)
+			end
 
       --drawRct(UI.x + 0.0149, UI.y + 0.800, 0.07715,0.0080,17, 160, 41,130) -- main-bar (hunger)
       --drawRct(UI.x + 0.0942, UI.y + 0.800, 0.07715,0.0080,0, 250, 255,130) -- main-bar (hunger)
