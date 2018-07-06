2ebba80347fb9d09f6e30e2602e745718b907ac4
James Lee Scott <me@jamesscott.pw>
2018-07-06 14:36:35 +0100

    :fsn_jobs: timer

--- resources/[fsn]/fsn_jobs/client.lua
+++ resources/[fsn]/fsn_jobs/client.lua
@@ -12,3 +12,11 @@ RegisterNetEvent('fsn_jobs:quit')
 AddEventHandler('fsn_jobs:quit', function()
   curjob = 'Unemployed'
 end)
+
+current_time = 0
+Citizen.CreateThread(function()
+  while true do
+    Citizen.Wait(1000)
+    current_time = current_time + 1
+  end
+end)
