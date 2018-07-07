54abcee95a48d712f9af296916f237b77c212e6f
James Lee Scott <me@jamesscott.pw>
2018-07-07 19:21:39 +0100

    :fsn_jobs: slowed down salary

--- resources/[fsn]/fsn_jobs/server.lua
+++ resources/[fsn]/fsn_jobs/server.lua
@@ -1,6 +1,6 @@
 Citizen.CreateThread(function()
   while true do
-    Citizen.Wait(300000)
+    Citizen.Wait(600000)
     print(':fsn_jobs: Paycheck time babies')
     TriggerClientEvent('fsn_jobs:paycheck', -1)
   end
