07669714798be930d8a736f9c06af16448c94bec
James Lee Scott <me@jamesscott.pw>
2018-08-02 14:44:38 +0100

    :fsn_police: MDT warrant

--- resources/[fsn]/fsn_police/MDT/mdt_server.lua
+++ resources/[fsn]/fsn_police/MDT/mdt_server.lua
@@ -35,3 +35,39 @@ AddEventHandler('fsn_police:database:CPIC:search', function(id)
     TriggerClientEvent('fsn_police:database:CPIC:search:result', src, results)
   end)
 end)
+
+RegisterServerEvent('fsn_police:MDT:warrant')
+AddEventHandler('fsn_police:MDT:warrant', function(tbl)
+  MySQL.Async.execute('INSERT INTO `fsn_warrants` (`war_id`, `suspect_name`, `officer_name`, `war_charges`, `war_times`, `war_fine`, `war_desc`, `war_status`, `war_date`) VALUES (NULL, @suspect_name, @officer_name, @war_charges, @war_times, @war_fine, @war_desc, @war_status, @war_time)', {
+    ['@suspect_name'] = tbl.suspectName,
+    ['@officer_name'] = tbl.officerName,
+    ['@war_charges'] = tbl.charges,
+    ['@war_times'] = tbl.jailTime,
+    ['@war_fine'] = tbl.jailFine,
+    ['@war_desc'] = tbl.situationDesc,
+    ['@war_status'] = 'ACTIVE',
+    ['@war_time'] = os.time()
+  }, function(rowsChanged) end)
+  TriggerClientEvent('chatMessage', source, '', {255,255,255}, '^7MDT |^0 Warrant submitted. Refresh warrants if required.')
+  TriggerClientEvent('fsn_police:911', -1, 'NIL', 'DISPATCH', 'A new warrant has been submitted for: ^*'..tbl.suspectName..'^r check your MDT for details.')
+end)
+
+RegisterServerEvent('fsn_police:MDT:requestwarrants')
+AddEventHandler('fsn_police:MDT:requestwarrants', function(name)
+  local src = source
+  if name ~= false then
+    name = '%'..name..'%'
+    MySQL.Async.fetchAll('SELECT * FROM `fsn_warrants` WHERE `suspect_name` LIKE @name AND `war_status` = "ACTIVE"', {['@name'] = name}, function(results)
+      TriggerClientEvent('fsn_police:MDT:receivewarrants', src, results)
+    end)
+  else
+    MySQL.Async.fetchAll('SELECT * FROM `fsn_warrants` WHERE `war_status` = "ACTIVE"', {['@name'] = name}, function(results)
+      TriggerClientEvent('fsn_police:MDT:receivewarrants', src, results)
+    end)
+  end
+end)
+
+RegisterServerEvent('fsn_police:MDTremovewarrant')
+AddEventHandler('fsn_police:MDTremovewarrant', function(id)
+    MySQL.Async.execute('DELETE FROM `fsn_warrants` WHERE `fsn_warrants`.`war_id` = @id', {['@id'] = id})
+end)
