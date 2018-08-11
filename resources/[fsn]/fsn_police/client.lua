c38acc18b92e08cd27aa409a22a1a8c5da612266
James Lee Scott <me@jamesscott.pw>
2018-08-11 16:03:26 +0100

    :fsn_police: remove AI

--- resources/[fsn]/fsn_police/client.lua
+++ resources/[fsn]/fsn_police/client.lua
@@ -606,6 +606,14 @@ AddEventHandler('fsn_police:toggleDrag', function(officer)
 end)
 --------------------------------------------- No wanted xx
 Citizen.CreateThread(function()
+  while true do
+    Citizen.Wait(0)
+    local playerPed = GetPlayerPed(-1)
+    local playerLocalisation = GetEntityCoords(playerPed)
+    ClearAreaOfCops(playerLocalisation.x, playerLocalisation.y, playerLocalisation.z, 400.0)
+  end
+end)
+Citizen.CreateThread(function()
     while true do
       Citizen.Wait(0)
       if GetPlayerWantedLevel(PlayerId()) ~= 0 then
