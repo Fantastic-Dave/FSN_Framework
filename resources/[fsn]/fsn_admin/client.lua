3e5c308f9bb4e1c4cb3c8ea00072ea7d05c1c3f0
James Lee Scott <me@jamesscott.pw>
2018-08-12 18:25:17 +0100

    :fsn_admin: Freeze ability

--- resources/[fsn]/fsn_admin/client.lua
+++ resources/[fsn]/fsn_admin/client.lua
@@ -8,3 +8,15 @@ RegisterNetEvent('fsn_admin:recieveXYZ')
 AddEventHandler('fsn_admin:recieveXYZ', function(xyz)
   SetEntityCoords(GetPlayerPed(-1), xyz.x, xyz.y, xyz.z, 1, 0, 0, 1)
 end)
+
+local frozen = false
+RegisterNetEvent('fsn_admin:FreezeMe')
+AddEventHandler('fsn_admin:FreezeMe', function(adminID)
+  if frozen then
+    TriggerEvent('chatMessage', '', {255,255,255}, '^1^*:fsn_admin:^r^0 You have been unfrozen by admin: '..GetPlayerName(adminID))
+    frozen = false
+  else
+    TriggerEvent('chatMessage', '', {255,255,255}, '^1^*:fsn_admin:^r^0 You have been frozen by admin: '..GetPlayerName(adminID))
+    frozen = true
+  end
+end)
