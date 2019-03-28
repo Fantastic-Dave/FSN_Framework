a29daa4c83972cfebf174e45abd7d7e0a3f597f2
James Lee Scott <me@jamesscott.pw>
2019-03-28 19:20:56 +0000

    :fsn_main: Fixed spawning in the air etc

--- resources/[fsn]/fsn_main/playermanager/client.lua
+++ resources/[fsn]/fsn_main/playermanager/client.lua
@@ -1,10 +1,36 @@
---TriggerServerEvent('fsn_main:validatePlayer')
+function showLoadingPrompt(showText, showTime, showType)
+  Citizen.CreateThread(function()
+    Citizen.Wait(0)
+    N_0xaba17d7ce615adbf("STRING") -- set type
+    AddTextComponentString(showText) -- sets the text
+    N_0xbd12f8228410d9b4(showType) -- show promt (types = 3)
+    Citizen.Wait(showTime) -- show time
+    N_0x10d373323e5b9c0d() -- remove promt
+  end)
+end
+
+local metime = 0
+AddEventHandler('playerSpawned', function()
+	DoScreenFadeOut(0)
+	Citizen.CreateThread(function()
+	  while true do
+		Citizen.Wait(0)
+		if metime > 10 then
+			if GetPlayerPed(-1) then
+			  DoScreenFadeIn(2000)
+			  TriggerEvent('fsn_main:charMenu')
+			  break
+			end
+		else
+			showLoadingPrompt("Loading your characters ("..metime..")...", 3000, 3)
+		end
+	  end
+	end)
+end)
+
 Citizen.CreateThread(function()
   while true do
-    Citizen.Wait(3000)
-    if GetPlayerPed(-1) then
-      TriggerEvent('fsn_main:charMenu')
-      break
-    end
+    Citizen.Wait(1000)
+	metime = metime + 1
   end
 end)
