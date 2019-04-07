3e3d29f30b925d0a36f0656132fa59093009c886
James Lee Scott <me@jamesscott.pw>
2019-04-07 12:05:53 +0100

    :fsn_store: started robberies

--- resources/[fsn]/fsn_store/client.lua
+++ resources/[fsn]/fsn_store/client.lua
@@ -42,7 +42,7 @@ RegisterNUICallback( "ButtonClick", function( data, cb )
 end )
 
 local stores = {
-	{x = -707.94940185547, y = -914.28442382813, z = 19.215589523315},
+  {x = -707.94940185547, y = -914.28442382813, z = 19.215589523315},
   {x = 25.816429138184, y = -1347.3413085938, z = 29.497024536133},
   {x = -48.42569732666, y = -1757.5212402344, z = 29.420993804932},
   {x = 374.22787475586, y = 326.03570556641, z = 103.56636810303},
@@ -88,6 +88,106 @@ Citizen.CreateThread( function()
 	end
 end )
 
-function chatPrint( msg )
-	TriggerEvent( 'chatMessage', "ActionMenu", { 255, 255, 255 }, msg )
-end
+local storekeepers = {
+	{x = -705.81408691406, y = -914.63317871094, z = 19.215587615967, h = 88.849708557129, ped = false}
+}	
+local robbing = false
+local robbingstart = 0
+local lastrob = 0
+local curtime = 0
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(1000)
+		curtime = curtime+1
+	end
+end)
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		for k,v in pairs(storekeepers) do
+			if GetDistanceBetweenCoords(v.x, v.y, v.z, GetEntityCoords(GetPlayerPed(-1)), true) < 50 then
+				if v.ped then
+					if GetDistanceBetweenCoords(GetEntityCoords(v.ped), v.x, v.y, v.z) > 2 and not IsPedFleeing(v.ped) and not IsPedInCombat(v.ped) then
+						RequestModel(416176080)
+						while not HasModelLoaded(416176080) do
+							Wait(1)
+						end
+						v.ped = CreatePed(5, 416176080, v.x, v.y, v.z, true, true)
+						SetEntityHeading(v.ped, v.h)
+					end
+					if IsPlayerFreeAiming(PlayerId()) and IsPlayerFreeAimingAtEntity(PlayerId(), v.ped) then
+						local quickmaff = lastrob + 1800
+						if quickmaff < curtime then
+							if robbing then
+								TaskStandStill(v.ped, 3000)
+								if not IsEntityPlayingAnim(v.ped, 'random@mugging3', "handsup_standing_base", 3) then
+									RequestAnimDict('random@mugging3')
+									TaskPlayAnim(v.ped, "random@mugging3", "handsup_standing_base", 4.0, -4, -1, 49, 0, 0, 0, 0)
+								end
+								local maff = robbingstart + 15
+								if maff < curtime then
+									robbing = false
+								end
+							elseif not IsPedInCombat(v.ped) then
+								if math.random(0,100) > 20 then
+									TaskStandStill(v.ped, 3000)
+									robbing = true
+									TriggerEvent('fsn_notify:displayNotification', 'Robbing...', 'centerLeft', 6000, 'info')
+									robbingstart = curtime
+									if not IsEntityPlayingAnim(v.ped, 'random@mugging3', "handsup_standing_base", 3) then
+										RequestAnimDict('random@mugging3')
+										TaskPlayAnim(v.ped, "random@mugging3", "handsup_standing_base", 4.0, -4, -1, 49, 0, 0, 0, 0)
+									end
+									if math.random(0,100) > 30 then
+										local pos = GetEntityCoords(GetPlayerPed(-1))
+										local coords = {
+										 x = pos.x,
+										 y = pos.y,
+										 z = pos.z
+										}
+										TriggerServerEvent('fsn_police:dispatch', coords, 12, '10-90 | ARMED ROBBERY IN PROGRESS')
+									end
+								else
+									TaskCombatPed(v.ped, GetPlayerPed(-1), 0, 16)
+									if math.random(0,100) > 30 then
+										local pos = GetEntityCoords(GetPlayerPed(-1))
+										local coords = {
+										 x = pos.x,
+										 y = pos.y,
+										 z = pos.z
+										}
+										TriggerServerEvent('fsn_police:dispatch', coords, 12, '10-90 | ARMED ROBBERY IN PROGRESS')
+									end
+									robbing = false
+								end
+							end
+						else
+							TriggerEvent('fsn_notify:displayNotification', 'You can\'t do that yet!', 'centerLeft', 6000, 'info')
+							local pos = GetEntityCoords(GetPlayerPed(-1))
+							local coords = {
+							 x = pos.x,
+							 y = pos.y,
+							 z = pos.z
+							}
+							TriggerServerEvent('fsn_police:dispatch', coords, 12, '10-90 | Attempted armed store robbery')
+							Citizen.Wait(2000)
+						end 
+					else
+						robbing = false
+					end
+				else
+					if GetClosestObjectOfType(v.x, v.y, v.z, 1.0, 416176080, false, false, false) then
+						v.ped = GetClosestObjectOfType(v.x, v.y, v.z, 1.0, 416176080, false, false, false)
+					else
+						RequestModel(416176080)
+						while not HasModelLoaded(416176080) do
+							Wait(1)
+						end
+						v.ped = CreatePed(5, 416176080, v.x, v.y, v.z, true, true)
+						SetEntityHeading(v.ped, v.h)
+					end
+				end
+			end
+		end
+	end
+end)
