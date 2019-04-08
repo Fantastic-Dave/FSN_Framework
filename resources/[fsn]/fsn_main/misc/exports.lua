5c60f50e4c8b706e21b1f09f7c12e38ead07d964
James Lee Scott <me@jamesscott.pw>
2019-04-08 12:55:15 +0100

    :fsn_main: new export 'fsn_FindPedNearbyCoords'

--- resources/[fsn]/fsn_main/misc/exports.lua
+++ resources/[fsn]/fsn_main/misc/exports.lua
@@ -13,3 +13,19 @@ function fsn_FindNearbyPed(Distance)
     EndFindPed(Handle)
     return TargetPed
 end
+
+function fsn_FindPedNearbyCoords(x,y,z,Distance)
+    local TargetPed
+    local Handle, Ped = FindFirstPed()
+    repeat
+        local DistanceBetween = GetDistanceBetweenCoords(x,y,z, GetEntityCoords(Ped), true)
+        if DoesEntityExist(Ped) and not IsPedAPlayer(Ped) and DistanceBetween <= Distance then
+            TargetPed = Ped
+        end
+
+    Success, Ped = FindNextPed(Handle)
+    until not Success
+
+    EndFindPed(Handle)
+    return TargetPed
+end
