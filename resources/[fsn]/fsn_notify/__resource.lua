c732f22363648c27b6b8d4b6b7f6bee1f27e0621
JamesSc0tt <jamesscott.js95@gmail.com>
2019-04-28 08:24:15 +0100

    :fsn_notify: serverside notify

--- resources/[fsn]/fsn_notify/__resource.lua
+++ resources/[fsn]/fsn_notify/__resource.lua
@@ -3,6 +3,7 @@ description "Simple Notification Script using https://notifyjs.com/"
 ui_page "html/index.html"
 
 client_script "cl_notify.lua"
+server_script "server.lua"
 
 export "SetQueueMax"
 export "SendNotification"
