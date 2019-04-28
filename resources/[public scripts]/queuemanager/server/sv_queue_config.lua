3380aac1412bc43417fd22d1e27ec6e8c25b2523
JamesSc0tt <jamesscott.js95@gmail.com>
2019-04-28 08:26:27 +0100

    :queuemanager: fix priority

--- resources/[public scripts]/queuemanager/server/sv_queue_config.lua
+++ resources/[public scripts]/queuemanager/server/sv_queue_config.lua
@@ -8,6 +8,7 @@ MySQL.ready(function()
 	MySQL.Async.fetchAll('SELECT * FROM `fsn_users` WHERE `priority` != 0', {}, function(res)
 		for k, usr in pairs(res) do
 			Config.Priority[usr.steamid] = usr.priority
+			Queue:AddPriority(usr.steamid, usr.priority, false)
 			print(':queuemanager: Adding priority('..usr.priority..') for user '..usr.name..'('..usr.steamid..')')
 		end
 	end)
