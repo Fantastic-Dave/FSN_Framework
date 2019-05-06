f40e7ab304b0b91719a666ff44d59fe9a7abd5b6
JamesSc0tt <jamesscott.js95@gmail.com>
2019-05-06 17:38:46 +0100

    :fsn_criminalmisc: race shit

--- resources/[fsn]/fsn_criminalmisc/streetracing/client.lua
+++ resources/[fsn]/fsn_criminalmisc/streetracing/client.lua
@@ -1,6 +1,8 @@
 local races = {}
 local inrace = false
+local myrace = 0
 local racestarted = false
+local raceblip = false
 
 RegisterNetEvent('fsn_criminalmisc:racing:createRace')
 AddEventHandler('fsn_criminalmisc:racing:createRace', function()
@@ -45,13 +47,57 @@ AddEventHandler('fsn_criminalmisc:racing:update', function(tbl)
 	races = tbl
 end)
 
+RegisterNetEvent('fsn_criminalmisc:racing:putmeinrace')
+AddEventHandler('fsn_criminalmisc:racing:putmeinrace', function(id, members)
+	for k, v in pairs(races) do
+		if v.race_id == id then
+			ClearGpsPlayerWaypoint()
+			SetWaypointOff()
+			if raceblip then
+				RemoveBlip(raceblip)
+			end
+			raceblip = AddBlipForCoord(v.finish.x, v.finish.y, 0.0)
+			SetBlipSprite(raceblip, 38)
+			SetBlipColour(raceblip, 3)
+			SetBlipRouteColour(raceblip, 1)
+			SetBlipRoute(raceblip,  true)
+			inrace = true
+			myrace = id
+		end
+	end
+	TriggerEvent('fsn_notify:displayNotification', 'You entered a race with '..#members..' other people', 'centerRight', 4000, 'success')
+end)
+
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(0)
-		if IsPedInAnyVehicle(GetPlayerPed(-1)) then
+		if IsPedInAnyVehicle(GetPlayerPed(-1)) then --and IsPedDiving(GetPlayerPed(-1)) then
 			for key, race in pairs(races) do
-				if GetDistanceBetweenCoords(race.start.x, race.start.y, race.start.z, GetEntityCoords(GetPlayerPed(-1)), false) < 10 then
-					
+				if GetDistanceBetweenCoords(race.start.x, race.start.y, race.start.z, GetEntityCoords(GetPlayerPed(-1)), false) < 20 and race.status == 'getready' then
+					if not inrace then
+						if exports["fsn_main"]:fsn_CanAfford(race.start.price) then
+							fsn_drawText3D(race.start.x, race.start.y, race.start.z, '[~g~E~w~] to join race ($'..race.start.price..')')
+							if IsControlJustPressed(0, 38) then
+								TriggerServerEvent('fsn_criminalmisc:racing:joinRace', race.race_id) 
+							end
+						else
+							fsn_drawText3D(race.start.x, race.start.y, race.start.z, '~r~You cannot afford to join the race!')
+						end
+					else
+						fsn_drawText3D(race.start.x, race.start.y, race.start.z, 'Race is starting shortly, do not move too far')
+					end
+				else
+					if inrace then
+						if not racestarted then
+							TriggerEvent('fsn_notify:displayNotification', 'You left the race.', 'centerRight', 4000, 'error')
+							if raceblip then
+								RemoveBlip(raceblip)
+							end
+							myrace = 0
+							inrace = false
+							racestarted = false
+						end
+					end
 				end
 			end
 		end
--- resources/[fsn]/fsn_criminalmisc/streetracing/server.lua
+++ resources/[fsn]/fsn_criminalmisc/streetracing/server.lua
@@ -2,8 +2,10 @@ local raceid = 1
 local races = {}
 
 RegisterServerEvent('fsn_criminalmisc:racing:newRace')
-AddEventHandler('fsn_criminalmisc:racing:newRace', function(cost, streetname, startx, starty, startz, x, y, racetoname)
+AddEventHandler('fsn_criminalmisc:racing:newRace', function(cost, streetname, startx, starty, startz, ex, why, racetoname)
+	local rid = raceid + 1
 	local race = {
+		race_id = rid,
 		starttime = os.time(),
 		start = {
 			x = startx,
@@ -11,8 +13,12 @@ AddEventHandler('fsn_criminalmisc:racing:newRace', function(cost, streetname, st
 			z = startz,
 			price = cost,
 		},
+		finish = {
+			x = ex,
+			y = why
+		},
 		status = 'getready',
-		members = 0
+		members = {},
 	}
 	table.insert(races, #races+1, race)
 	TriggerClientEvent('fsn_criminalmisc:racing:update', -1, races)
@@ -22,15 +28,41 @@ AddEventHandler('fsn_criminalmisc:racing:newRace', function(cost, streetname, st
       to_number = -1,
       message = 'A new race has been started at '..streetname..' to '..racetoname..' get there within 5 minutes to take part!'
     })
-	print('new race being started at '..streetname..' to '..racetoname)
+end)
+
+RegisterServerEvent('fsn_criminalmisc:racing:joinRace')
+AddEventHandler('fsn_criminalmisc:racing:joinRace', function(id)
+	for k, v in pairs(races) do
+		if v.race_id == id then
+			if v.status == 'getready' then
+				table.insert(v.members, #v.members+1, source)
+				TriggerClientEvent('fsn_criminalmisc:racing:putmeinrace', source, v.race_id, v.members)
+			else
+				TriggerClientEvent('fsn_notify:displayNotification', source, 'Too late!', 'centerLeft', 4000, 'error')
+			end
+		end
+	end
 end)
 
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(0)
 		for k,v in pairs(races) do
-			if v.status ~= 'completed' then
-			
+			if v.status == 'getready' then			
+				local maff = v.starttime + 300
+				if maff <= os.time() then
+					for k, ply in pairs(v.members) do
+						TriggerClientEvent('fsn_notify:displayNotification', ply, '<b>THE RACE HAS STARTED GO GO GO', 'centerLeft', 4000, 'info')
+						TriggerClientEvent('fsn_phone:recieveMessage', ply, {
+						  sender = 'Racerz',
+						  from_number = 123456,
+						  to_number = -1,
+						  message = 'The race has started. Go go go!'
+						})
+					end
+					v.status = 'inprogress'
+					TriggerClientEvent('fsn_criminalmisc:racing:update', -1, races)
+				end
 			end
 		end
 	end
