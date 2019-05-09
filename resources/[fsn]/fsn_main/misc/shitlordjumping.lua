f54d99e90eb622e08e2fb216e5d7cfdd396bcf9d
JamesSc0tt <jamesscott.js95@gmail.com>
2019-05-09 18:06:37 +0100

    :fsn_main: shitlordjumping actually fixed

--- resources/[fsn]/fsn_main/misc/shitlordjumping.lua
+++ resources/[fsn]/fsn_main/misc/shitlordjumping.lua
@@ -1,18 +1,26 @@
-local lastJump = 0
-local jumped = false
+local canJump = true
 
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(1)
 		if IsPedJumping(GetPlayerPed(-1)) then
-			local maff = lastJump + 5
-			if maff >= fsn_GetTime() and jumped == false then
-				lastJump = fsn_GetTime()
+			if canJump then
+				Citizen.Wait(1000)
+				canJump = false
+				Citizen.Wait(3000)
+				canJump = true
+			end 			
+		end
+	end
+end)
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(1)
+		if IsPedJumping(GetPlayerPed(-1)) then
+			if not canJump then
 				SetPedToRagdoll(GetPlayerPed(-1), 1, 1000, 0, 0, 0, 0)
-				jumped = true
-				Citizen.Wait(5000)
-				jumped = false
-			end
+			end 			
 		end
 	end
 end)
