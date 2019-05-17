c5191b35b278884905d3c2ede26ce814f53d6bff
JamesSc0tt <jamesscott.js95@gmail.com>
2019-05-17 20:20:48 +0100

    :tokovoip_extras: new shit

---
+++ resources/[public scripts]/tokovoip_extras/c_calls.lua
@@ -0,0 +1,68 @@
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+    NetworkSetVoiceActive(false)
+	end
+end)
+
+--PlaySound(-1, "10_SEC_WARNING", "HUD_MINI_GAME_SOUNDSET", 0, 0, 1)
+--TriggerClientEvent('fsn_phone:call:effects:calling', src)
+--TriggerClientEvent('fsn_phone:call:effects:called', src)
+local beingCalled = false
+local rings = 0
+local isCalling = false
+local isInCall = false
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		if isInCall then
+			while not HasAnimDictLoaded('cellphone@') do
+				RequestAnimDict('cellphone@')
+				Citizen.Wait(5)
+			end
+			if not IsEntityPlayingAnim(GetPlayerPed(-1), 'cellphone@', 'cellphone_call_listen_base', 3) and not IsPedRagdoll(GetPlayerPed(-1)) then
+				TaskPlayAnim(GetPlayerPed(-1), 'cellphone@', 'cellphone_call_listen_base', 8.0, 1.0, -1, 49, 1.0, 0, 0, 0)
+			end
+		end
+		if beingCalled then
+			rings = rings + 1
+			if rings > 5 then
+				ExecuteCommand('/decline')
+				TriggerEvent('fsn_notify:displayNotification', 'Call rejected', 'centerLeft', 3000, 'error')
+			else
+				TriggerEvent('fsn_phone:sound', 'ringtone')
+				Citizen.Wait(17000)
+			end
+		end
+		if isCalling then
+			TriggerEvent('fsn_phone:sound', 'ringtone')
+			--Citizen.Wait(17000)
+		end
+	end
+end)
+
+RegisterNetEvent('fsn_phone:call:effects:called')
+AddEventHandler('fsn_phone:call:effects:called', function()
+	beingCalled = true
+	rings = 0
+end)
+
+RegisterNetEvent('fsn_phone:call:effects:calling')
+AddEventHandler('fsn_phone:call:effects:calling', function()
+	isCalling = true
+	isInCall = true
+end)
+
+RegisterNetEvent('fsn_phone:call:effects:stop')
+AddEventHandler('fsn_phone:call:effects:stop', function()
+	isCalling = false
+	beingCalled = false
+end)
+
+RegisterNetEvent('fsn_phone:call:effects:isInCall')
+AddEventHandler('fsn_phone:call:effects:isInCall', function(state)
+	isInCall = state
+	if state == false then
+		ClearPedTasks(GetPlayerPed(-1))
+	end
+end)
---
+++ resources/[public scripts]/tokovoip_extras/s_calls.lua
@@ -0,0 +1,180 @@
+local channelid = 3 
+local calls = {}
+--[[
+	{
+		caller = 999,
+		calling = 999,
+		time = os.time(),
+		members = {},
+		status = 'calling'
+	}
+	
+]]
+
+function fsn_SplitString(inputstr, sep)
+    if sep == nil then
+        sep = "%s"
+    end
+    local t={} ; i=1
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+        t[i] = str
+        i = i + 1
+    end
+    return t
+end
+
+function isBusy(player)
+	local number = exports.fsn_main:fsn_GetPlayerPhoneNumber(tonumber(player))
+	for k,call in pairs(calls) do
+		if call.status ~= 'finished' then
+			if call.caller == number or call.calling == number then
+				return true
+			end
+		end
+	end
+	return false
+end
+
+RegisterServerEvent('fsn_phone:call:startCalling')
+AddEventHandler('fsn_phone:call:startCalling', function(src, toNumber)
+	if isBusy(src) then
+		TriggerClientEvent('fsn_notify:displayNotification', src, 'You are currently on a phonecall', 'centerRight', 3000, 'error')
+	end
+	local ply = exports.fsn_main:fsn_GetPlayerFromPhoneNumber(tonumber(toNumber))
+	if ply ~= 0 then
+		if ply == src then
+			TriggerClientEvent('fsn_notify:displayNotification', src, 'You cannot call yourself..', 'centerRight', 3000, 'error')
+			return
+		end
+		if not isBusy(ply) then
+			TriggerClientEvent('fsn_notify:displayNotification', src, 'Calling <b>'..tonumber(toNumber)..'</b>...', 'centerRight', 3000, 'info')
+			TriggerClientEvent('fsn_phone:call:effects:calling', src)
+			TriggerClientEvent('fsn_phone:call:effects:called', ply)
+			
+			TriggerClientEvent('chatMessage', ply, '', {255,255,255}, '^1^*:fsn_phone:^0^r You are being called by '..exports.fsn_main:fsn_GetPlayerPhoneNumber(tonumber(src))..'. Use /answer or /decline')
+			TriggerClientEvent('fsn_notify:displayNotification', ply, 'You are being called by '..exports.fsn_main:fsn_GetPlayerPhoneNumber(tonumber(src))..'. Use /answer or /decline', 'centerRight', 3000, 'info')
+			
+			--TriggerClientEvent('fsn_phone:sound', ply, 'ringtone')
+			--TriggerClientEvent('fsn_phone:sound', src, 'calling')
+			channelid = channelid +1
+			local tbl = {
+				channel = channelid,
+				caller = exports.fsn_main:fsn_GetPlayerPhoneNumber(tonumber(src)),
+				calling = tonumber(toNumber),
+				time = os.time(),
+				members = {src, ply},
+				status = 'calling'
+			}
+			table.insert(calls, #calls+1, tbl)
+		else
+			TriggerClientEvent('fsn_notify:displayNotification', src, '<b>'..tonumber(toNumber)..'</b> is busy, try again later!', 'centerRight', 3000, 'error')
+		end
+	else
+		TriggerClientEvent('fsn_notify:displayNotification', src, 'No player found with number <b>'..tonumber(toNumber), 'centerRight', 3000, 'error')		
+	end
+end)
+
+RegisterServerEvent('fsn_phone:call:startCall')
+AddEventHandler('fsn_phone:call:startCall', function(callid)
+	local v = calls[callid]
+	--for k, v in pairs(calls) do
+		v.status = 'incall'
+		local called = exports.fsn_main:fsn_GetPlayerFromPhoneNumber(tonumber(v.calling))
+		local caller = exports.fsn_main:fsn_GetPlayerFromPhoneNumber(tonumber(v.caller))
+		
+		TriggerClientEvent('fsn_phone:call:effects:isInCall', called, true)
+		TriggerClientEvent('fsn_phone:call:effects:isInCall', caller, true)
+		
+		TriggerClientEvent('chatMessage', called, '', {255,255,255}, '^1^*:fsn_phone:^0^r Call started. Use CAPSLOCK to talk on the phone or use /hangup to end the call')
+		TriggerClientEvent('chatMessage', caller, '', {255,255,255}, '^1^*:fsn_phone:^0^r Call started. Use CAPSLOCK to talk on the phone or use /hangup to end the call')
+		
+		TriggerClientEvent('TokoVoip:addPlayerToRadio', called, v.channel, GetPlayerName(called))
+		TriggerClientEvent('TokoVoip:addPlayerToRadio', caller, v.channel, GetPlayerName(caller))
+	--end
+end)
+
+RegisterServerEvent('fsn_phone:call:endCall')
+AddEventHandler('fsn_phone:call:endCall', function(callid)
+	local v = calls[callid]
+	v.status = 'finished'
+	local called = exports.fsn_main:fsn_GetPlayerFromPhoneNumber(tonumber(v.calling))
+	local caller = exports.fsn_main:fsn_GetPlayerFromPhoneNumber(tonumber(v.caller))
+		
+	TriggerClientEvent('fsn_phone:call:effects:isInCall', called, false)
+	TriggerClientEvent('fsn_phone:call:effects:isInCall', caller, false)
+	
+	TriggerClientEvent('fsn_notify:displayNotification', called, 'Call ended', 'centerRight', 5000, 'info')
+	TriggerClientEvent('fsn_notify:displayNotification', caller, 'Call ended', 'centerRight', 5000, 'info')
+	
+	TriggerClientEvent('TokoVoip:removePlayerFromRadio', called, v.channel)
+	TriggerClientEvent('TokoVoip:removePlayerFromRadio', caller, v.channel)
+end)
+
+RegisterServerEvent('fsn_phone:call:answerCall')
+AddEventHandler('fsn_phone:call:answerCall', function(src)
+	for k,v in pairs(calls) do
+		if v.status == 'calling' then
+			if v.calling == exports.fsn_main:fsn_GetPlayerPhoneNumber(tonumber(src)) then
+				TriggerEvent('fsn_phone:call:startCall', k)
+				TriggerClientEvent('fsn_phone:call:effects:stop', src)
+				TriggerClientEvent('fsn_phone:call:effects:stop', exports.fsn_main:fsn_GetPlayerFromPhoneNumber(tonumber(v.caller)))
+			end
+		end
+	end
+end)
+
+RegisterServerEvent('fsn_phone:call:declineCall')
+AddEventHandler('fsn_phone:call:declineCall', function(src)
+	for k,v in pairs(calls) do
+		if v.status == 'calling' then
+			if v.calling == exports.fsn_main:fsn_GetPlayerPhoneNumber(tonumber(src)) then
+				v.status = 'finished'
+				
+				TriggerClientEvent('fsn_phone:call:effects:stop', src)
+				TriggerClientEvent('fsn_phone:call:effects:stop', exports.fsn_main:fsn_GetPlayerFromPhoneNumber(tonumber(v.caller)))
+				TriggerClientEvent('fsn_phone:call:effects:isInCall', exports.fsn_main:fsn_GetPlayerFromPhoneNumber(tonumber(v.caller)), false)
+				
+				TriggerClientEvent('fsn_notify:displayNotification', exports.fsn_main:fsn_GetPlayerFromPhoneNumber(tonumber(v.caller)), '<b>'..tonumber(v.calling)..'</b> is busy, try again later!', 'centerRight', 3000, 'error')
+				TriggerClientEvent('fsn_notify:displayNotification', exports.fsn_main:fsn_GetPlayerFromPhoneNumber(tonumber(v.calling)), 'Call declined', 'centerRight', 3000, 'error')
+			end
+		end
+	end
+end)
+
+
+AddEventHandler('chatMessage', function(source, auth, msg)
+	local split = fsn_SplitString(msg, ' ')
+	if split[1] == '/call' then
+		if split[2] and tonumber(split[2]) then
+			TriggerEvent('fsn_phone:call:startCalling', source, tonumber(split[2]))
+		else
+			TriggerClientEvent('chatMessage', source, '', {255,255,255}, '^1^*:fsn_phone:^0^r There was an issue with the number you provided. (Usage: /call {number})')
+		end 
+	end
+	if split[1] == '/answer' then
+		TriggerEvent('fsn_phone:call:answerCall', source)
+	end
+	if split[1] == '/decline' then
+		TriggerEvent('fsn_phone:call:declineCall', source)
+	end
+	if split[1] == '/hangup' then
+		for k, v in pairs(calls) do
+			if v.status == 'incall' then
+				if v.calling == exports.fsn_main:fsn_GetPlayerPhoneNumber(tonumber(source)) or v.caller == exports.fsn_main:fsn_GetPlayerPhoneNumber(tonumber(source)) then
+					TriggerEvent('fsn_phone:call:endCall', k)
+					return
+				end
+			end
+		end
+		TriggerClientEvent('fsn_notify:displayNotification', source, 'Either you are not in a call, or the system forgot about your call and you need to relog.', 'centerRight', 3000, 'error')
+	end
+end)
+
+function table.contains(table, element)
+  for _, value in pairs(table) do
+    if value[1] == element then
+      return true
+    end
+  end
+  return false
+end
