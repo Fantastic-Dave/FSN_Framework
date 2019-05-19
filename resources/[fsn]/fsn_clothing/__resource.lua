6eed7497fbd136e133b2862472d9a5cbf9cf3fbd
JamesSc0tt <jamesscott.js95@gmail.com>
2019-05-19 18:07:50 +0100

    :fsn_clothing: new store

--- resources/[fsn]/fsn_clothing/__resource.lua
+++ resources/[fsn]/fsn_clothing/__resource.lua
@@ -1,2 +1,14 @@
-client_script 'client.lua'
-client_script 'skins.lua'
+resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
+description 'Model Menu v3 by Frazzle :D'
+client_scripts {
+	'gui.lua',
+	'client.lua',
+	'config.lua',
+}
+
+server_script 'server.lua'
+
+-- exports
+exports {
+	"isClothingOpen"
+}
