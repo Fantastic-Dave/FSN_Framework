d97f1946581a063817434428f196a2cb97ab846c
xLiquidx <39743473+xLiquidx@users.noreply.github.com>
2019-05-18 22:15:56 -0400

    :fsn_carstore: lynx 5k cheaper

--- resources/[fsn]/fsn_carstore/vehshop.lua
+++ resources/[fsn]/fsn_carstore/vehshop.lua
@@ -98,7 +98,7 @@ local vehshop = {
 				{name = "Jester", costs = 400000, description = {}, model = "jester"},
 				{name = "Jester(Racecar)", costs = 575000, description = {}, model = "jester2"},
 				{name = "Kuruma", costs = 135000, description = {}, model = "kuruma"},
-				{name = "Lynx", costs = 180000, description = {}, model = "lynx"},
+				{name = "Lynx", costs = 175000, description = {}, model = "lynx"},
 				{name = "Massacro", costs = 299999, description = {}, model = "massacro"},
 				{name = "Massacro 2 Sports", costs = 335000, description = {}, model = "massacro2"},
 				{name = "Neon", costs = 135000, description = {}, model = "neon"},
