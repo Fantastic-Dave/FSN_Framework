e56389561cadc8e10acd679b8cfd7960c22c131b
JamesSc0tt <jamesscott.js95@gmail.com>
2019-05-21 22:17:20 +0100

    :fsn_criminalmisc: update resource

--- resources/[fsn]/fsn_criminalmisc/__resource.lua
+++ resources/[fsn]/fsn_criminalmisc/__resource.lua
@@ -23,6 +23,12 @@ client_script 'lockpicking/client.lua'
 
 -- weapon info
 client_script 'weaponinfo/client.lua'
+client_script 'weaponinfo/weapon_list.lua'
+server_script 'weaponinfo/server.lua'
 
 -- drugs
 client_script 'drugs/client.lua'
+
+exports {
+	"GetWeapons"
+}
