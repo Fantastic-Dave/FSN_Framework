f0c8765969a480c28d93e5d45a11d67b6eb79c48
JamesSc0tt <jamesscott.js95@gmail.com>
2019-05-25 14:02:32 +0100

    :fsn_clothing: debug

--- resources/[fsn]/fsn_clothing/server.lua
+++ resources/[fsn]/fsn_clothing/server.lua
@@ -1,6 +1,6 @@
---[[
 RegisterServerEvent("clothes:firstspawn")
 AddEventHandler("clothes:firstspawn",function()
+	print('source has no clothes, sending some')
 	local source = source
 	local default_models = {1413662315,-781039234,1077785853,2021631368,1423699487,1068876755,2120901815,-106498753,131961260,-1806291497,1641152947,115168927,330231874,-1444213182,1809430156,1822107721,2064532783,-573920724,-782401935,808859815,-1106743555,-1606864033,1004114196,532905404,1699403886,-1656894598,1674107025,-88831029,-1244692252,951767867,1388848350,1090617681,379310561,-569505431,-1332260293,-840346158}
 	local models = {}
@@ -14,7 +14,7 @@ AddEventHandler("clothes:firstspawn",function()
 	}
 	TriggerClientEvent("clothes:spawn", source, models[identifier])
 end)
-]]--
+
 RegisterServerEvent('fsn_clothing:requestDefault')
 AddEventHandler('fsn_clothing:requestDefault', function()
 	
