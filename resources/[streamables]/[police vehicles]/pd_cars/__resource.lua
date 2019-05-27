e89fc206fd674f287a8fca4cea9b6f13959e4912
JamesSc0tt <jamesscott.js95@gmail.com>
2019-05-27 21:41:56 +0100

    :fsn_handling: :pd_cars: fixed finn's fuckery

--- resources/[streamables]/[police vehicles]/pd_cars/__resource.lua
+++ resources/[streamables]/[police vehicles]/pd_cars/__resource.lua
@@ -4,8 +4,10 @@ files {
 	'vehicles.meta',
 	'carcols.meta',
 	'carvariations.meta',
+	'handling.meta'
 }
 
+data_file 'HANDLING_FILE' 'handling.meta'
 data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
 data_file 'CARCOLS_FILE' 'carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
