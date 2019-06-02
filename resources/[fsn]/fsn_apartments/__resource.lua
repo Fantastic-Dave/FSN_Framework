99310e3105aa806a5086520e19ab443b495d8209
JamesSc0tt <jamesscott.js95@gmail.com>
2019-06-02 12:14:39 +0100

    :fsn_apartments: storage gui init

--- resources/[fsn]/fsn_apartments/__resource.lua
+++ resources/[fsn]/fsn_apartments/__resource.lua
@@ -1,5 +1,14 @@
 resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 
+ui_page "gui/ui.html"
+
+files {
+	"gui/ui.html",
+	"gui/ui.js",
+	"gui/ui.css"
+}
+
+
 client_script 'client.lua'
 server_script '@mysql-async/lib/MySQL.lua'
 server_script 'server.lua'
