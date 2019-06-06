80aa76d44d5568aec5b52e0370698a2162e94a2c
James Lee Scott <me@jamesscott.pw>
2019-06-06 10:36:22 +0100

    :fsn_cargarage: improve my fix

--- resources/[fsn]/fsn_cargarage/client.lua
+++ resources/[fsn]/fsn_cargarage/client.lua
@@ -240,7 +240,14 @@ Citizen.CreateThread(function()
 			if GetDistanceBetweenCoords(grg.pos.x,grg.pos.y,grg.pos.z,GetEntityCoords(GetPlayerPed(-1)), true) < 10 then
         DrawMarker(1,grg.pos.x,grg.pos.y,grg.pos.z-1,0,0,0,0,0,0,5.8, 5.8, 0.5,0,155,255,175,0,0,0,0)
         if GetDistanceBetweenCoords(grg.pos.x,grg.pos.y,grg.pos.z,GetEntityCoords(GetPlayerPed(-1)), true) < 5.8 then
-					local vehicle = fsn_IsVehicleHere(grg.pos.x,grg.pos.y,grg.pos.z,5.8)
+					local vehicle false
+					if IsPedInAnyVehicle(GetPlayerPed(-1)) then
+						if GetVehiclePedIsUsing(GetPlayerPed(-1)) then
+							vehicle = GetVehiclePedIsUsing(GetPlayerPed(-1))		
+						end
+					else
+						vehicle = fsn_IsVehicleHere(grg.pos.x,grg.pos.y,grg.pos.z,5.8)
+					end
 					if vehicle then
 						SetTextComponentFormat("STRING")
 						AddTextComponentString("Press ~INPUT_CELLPHONE_SELECT~ to ~r~return~w~ your vehicle")
