a90571466ce792e0b12b100bbc6a170c2f9e313f
Kng <kngrektor@gmail.com>
2019-06-16 16:35:44 +0200

    :fsn_main: More cleanup, fsn_debug and thread time warnings Execute "fsn_debug true" to enable thread time warnings

--- resources/[fsn]/fsn_main/__resource.lua
+++ resources/[fsn]/fsn_main/__resource.lua
@@ -1,12 +1,16 @@
 resource_manifest_version "05cfa83c-a124-4cfa-a768-c24a5811d8f9"
 
+client_script "debug/cl_subframetime.js"
+client_script "debug/sh_scheduler.lua"
+server_script "debug/sh_scheduler.lua"
+client_script "debug/sh_debug.lua"
+server_script "debug/sh_debug.lua"
 -- Client scripts
 client_script 'initial/client.lua'
 client_script 'money/client.lua'
 client_script 'hud/client.lua'
 client_script 'hud/playerhud.lua'
 client_script 'playermanager/client.lua'
---client_script 'imanager/client.lua'
 client_script 'misc/exports.lua'
 client_script 'misc/blips.lua'
 client_script 'misc/weapondrop.lua'
@@ -30,7 +34,6 @@ server_script '@mysql-async/lib/MySQL.lua'
 server_script 'initial/server.lua'
 server_script 'money/server.lua'
 server_script 'playermanager/server.lua'
---server_script 'imanager/server.lua'
 server_script 'misc/logging.lua'
 
 
