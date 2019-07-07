48df01b435d5db007d51e836e16d589082b578b9
JamesSc0tt <jamesscott.js95@gmail.com>
2019-07-07 18:21:12 +0100

    :fsn_inventory: painkillers, morphine, bandage

--- resources/[fsn]/fsn_inventory/items.lua
+++ resources/[fsn]/fsn_inventory/items.lua
@@ -1,4 +1,24 @@
 items_table = {
+  ["painkillers"] = {
+    display_name = 'Pain Killers',
+    weight = 0.4,
+    desc = 'Use this to repair yourself.',
+    use = function()
+		TriggerEvent('mythic_hospital:client:UsePainKiller', 2)
+    end,
+    modelhash = -2140074399,
+    price = 250
+  },
+  ["morphine"] = {
+    display_name = 'Morphine',
+    weight = 0.4,
+    desc = 'Use this to repair yourself.',
+    use = function()
+		TriggerEvent('mythic_hospital:client:UsePainKiller', 6)
+    end,
+    modelhash = -2140074399,
+    price = 250
+  },
   ["bandage"] = {
     display_name = 'Bandage',
     weight = 0.4,
@@ -13,6 +33,8 @@ items_table = {
 			  TaskPlayAnim(GetPlayerPed(-1), "oddjobs@assassinate@guard", "unarmed_fold_arms", 8.0, 1.0, 2500, 2, 0, 0, 0, 0 )  
 			  Citizen.Wait(1500)
 			  SetEntityHealth(GetPlayerPed(-1), GetEntityHealth(GetPlayerPed(-1))+15)
+			  TriggerEvent('mythic_hospital:client:FieldTreatBleed')
+
 	  else
 		TriggerEvent('fsn_notify:displayNotification', 'You don\'t need to use a bandage!<br>Visit an EMS personnel or a hospital to heal more.', 'centerLeft', 3500, 'error')
 	  end
@@ -66,7 +88,8 @@ items_table = {
     weight = 0.3,
     desc = 'Time to make something meaty.',
     use = function()
-    end,
+		TriggerEvent('mythic_hospital:client:UseAdrenaline')
+	end,
     modelhash = 1302435108
   },
   ["dirty_money"] = {
