5c7c2ecf7b597e625d7a4b13ffdbd937489494d7
JamesSc0tt <jamesscott.js95@gmail.com>
2019-07-10 22:51:01 +0100

    :fsn_main: new util SplitString

---
+++ resources/[fsn]/fsn_main/sv_utils.lua
@@ -0,0 +1,13 @@
+Util = {}
+
+function Util.SplitString(inputstr, sep)
+    if sep == nil then
+        sep = "%s"
+    end
+    local t={} ; i=1
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+        t[i] = str
+        i = i + 1
+    end
+    return t
+end
