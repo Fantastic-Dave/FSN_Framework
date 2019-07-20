e3441c0d9f3880a66f05f63c75dd6f079fd37642
SilverNick <32308171+SilverNick@users.noreply.github.com>
2019-07-19 21:44:01 -0400

    Removed Pistol whiping thanks Volgan

--- resources/[fsn]/fsn_weaponcontrol/client.lua
+++ resources/[fsn]/fsn_weaponcontrol/client.lua
@@ -286,3 +286,14 @@ Citizen.CreateThread(function()
 		end
 	end
 end)
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        local ped = PlayerPedId()
+                if IsPedArmed(ped, 6) then
+            DisableControlAction(1, 140, true)
+                DisableControlAction(1, 141, true)
+                DisableControlAction(1, 142, true)
+        end
+    end
+end)
