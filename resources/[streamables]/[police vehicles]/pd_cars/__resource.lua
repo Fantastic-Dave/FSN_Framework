095523cc6b8c8c42a3ad7270fc0a97e6880349c3
JamesSc0tt <jamesscott.js95@gmail.com>
2019-07-21 13:08:30 +0100

    :fsn: police vehicles

--- resources/[streamables]/[police vehicles]/pd_cars/__resource.lua
+++ resources/[streamables]/[police vehicles]/pd_cars/__resource.lua
@@ -1,15 +1,13 @@
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 
 files {
-	'vehicles.meta',
-	'carcols.meta',
-	'carvariations.meta',
-	'handling.meta'
+	'data/vehicles.meta',
+	'data/carcols.meta',
+	'data/carvariations.meta',
 }
 
-data_file 'HANDLING_FILE' 'handling.meta'
-data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
-data_file 'CARCOLS_FILE' 'carcols.meta'
-data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
 
-client_script 'vehicle_names.lua'
+client_script 'data/vehicle_names.lua'
