daf748306ee2a11cb26b991ca33c33c510fc2b87
JamesSc0tt <jamesscott.js95@gmail.com>
2019-08-04 19:38:30 +0100

    :fsn_ems: carry downed person

--- resources/[fsn]/fsn_ems/__resource.lua
+++ resources/[fsn]/fsn_ems/__resource.lua
@@ -1,10 +1,15 @@
 --client_script 'debug_kng.lua'
+-- GLOBAL UTILS
+client_script '@fsn_main/cl_utils.lua'
+server_script '@fsn_main/sv_utils.lua'
 
 client_script 'client.lua'
 client_script 'cl_advanceddamage.lua'
+client_script 'cl_carrydead.lua'
 client_script 'beds/client.lua'
 
 server_script 'server.lua'
+server_script 'sv_carrydead.lua'
 server_script 'beds/server.lua'
 
 exports({
@@ -12,4 +17,5 @@ exports({
   'fsn_EMSDuty',
   'fsn_getEMSLevel',
   'fsn_Airlift',
+  'carryingWho'
 })
