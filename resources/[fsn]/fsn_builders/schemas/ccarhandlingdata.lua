49ec5232c898a52ba58a29909250d2bc0ce4b32f
Kng <kngrektor@gmail.com>
2019-08-08 19:18:57 +0200

    :fsn_builders: Handling builder

---
+++ resources/[fsn]/fsn_builders/schemas/ccarhandlingdata.lua
@@ -0,0 +1,8 @@
+Field "float" "fBackEndPopUpCarImpulseMult"
+	Default (0.05)
+
+Field "float" "fBackEndPopUpBuildingImpulseMult"
+	Default (0.03)
+
+Field "float" "fBackEndPopUpMaxDeltaSpeed"
+	Default (0.6)
