9921a68149730ea5d5c1efb0f555432c19649b36
JamesSc0tt <jamesscott.js95@gmail.com>
2019-08-08 01:11:35 +0100

    :fsn_bankrobbery: hotpatch

--- resources/[fsn]/fsn_bankrobbery/__resource.lua
+++ resources/[fsn]/fsn_bankrobbery/__resource.lua
@@ -3,7 +3,7 @@ client_script '@fsn_main/cl_utils.lua'
 server_script '@fsn_main/sv_utils.lua'
 
 client_script 'client.lua'
-client_script 'trucks.lua'
+--client_script 'trucks.lua'
 server_script 'server.lua'
 
 client_script 'cl_frontdesks.lua'
