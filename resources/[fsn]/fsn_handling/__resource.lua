2cc0c925d7db5986cf4c5a78a90428334727d419
Kng <kngrektor@gmail.com>
2019-08-08 21:06:46 +0200

    :fsn_handling: Ported SCHAFTER2, SCHAFTER3, SCHAFTER4 to fsn_builders

--- resources/[fsn]/fsn_handling/__resource.lua
+++ resources/[fsn]/fsn_handling/__resource.lua
@@ -1,7 +1,8 @@
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 
-files {
-	'data/handling.meta'
-}
+dependency "fsn_builders"
+fsn_handling "src/schafter.lua"
 
+files { 'out/handling.meta', 'data/handling.meta' }
+data_file 'HANDLING_FILE' 'out/handling.meta'
 data_file 'HANDLING_FILE' 'data/handling.meta'
