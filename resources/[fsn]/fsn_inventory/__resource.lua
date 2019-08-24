e4e69916389656c1358e9f4a0dba8b35eaf75481
JamesSc0tt <jamesscott.js95@gmail.com>
2019-08-24 18:53:29 +0100

    :fsn_inventory: new inventory dropping mech

--- resources/[fsn]/fsn_inventory/__resource.lua
+++ resources/[fsn]/fsn_inventory/__resource.lua
@@ -1,3 +1,7 @@
+-- GLOBAL UTILS
+client_script '@fsn_main/cl_utils.lua'
+server_script '@fsn_main/sv_utils.lua'
+
 client_script 'client.lua'
 client_script 'items.lua'
 
@@ -24,10 +28,110 @@ client_script '_item_misc/drug_selling.lua'
 client_script '_item_misc/dm_laundering.lua'
 client_script '_item_misc/burger_store.lua'
 
-ui_page "gui/index.html"
-
+---------------------------------------------------- new script stuff below
+	client_script 'cl_presetItems.lua'
+	client_script 'cl_uses.lua'
+	client_script 'cl_inventory.lua'
+	client_script 'cl_dropping.lua'
+	server_script 'sv_inventory.lua'
+	server_script 'sv_dropping.lua'
+-- new gui
+ui_page "html/ui.html"
 files {
-	"gui/index.html",
-	"gui/index.js",
-	"gui/index.css"
+  "html/ui.html",
+  "html/css/ui.css",
+  "html/css/jquery-ui.css",
+  "html/js/inventory.js",
+  "html/js/config.js",
+  -- JS LOCALES
+  "html/locales/cs.js",
+  "html/locales/en.js",
+  "html/locales/fr.js",
+  -- IMAGES
+  "html/img/bullet.png",
+  -- ICONS
+  "html/img/items/id.png",
+  "html/img/items/evidence.png",
+  "html/img/items/vine.png",
+  "html/img/items/jus_raisin.png",
+  "html/img/items/grand_cru.png",
+  "html/img/items/raisin.png",
+  "html/img/items/fish.png",
+  "html/img/items/fishingrod.png",
+  "html/img/items/bait.png",
+  "html/img/items/bandage.png",
+  "html/img/items/beer.png",
+  "html/img/items/binoculars.png",
+  "html/img/items/bread.png",
+  "html/img/items/cannabis.png",
+  "html/img/items/cigarette.png",
+  "html/img/items/clip.png",
+  "html/img/items/cocacola.png",
+  "html/img/items/coffee.png",
+  "html/img/items/coke.png",
+  "html/img/items/coke_pooch.png",
+  "html/img/items/meth.png",
+  "html/img/items/meth_pooch.png",
+  "html/img/items/opium.png",
+  "html/img/items/opium_pooch.png",
+  "html/img/items/weed.png",
+  "html/img/items/gold.png",
+  "html/img/items/icetea.png",
+  "html/img/items/sandwich.png",
+  "html/img/items/papers.png",
+  "html/img/items/hamburger.png",
+  "html/img/items/wine.png",
+  "html/img/items/cash.png",
+  "html/img/items/chocolate.png",
+  "html/img/items/iron.png",
+  "html/img/items/jewels.png",
+  "html/img/items/medikit.png",
+  "html/img/items/tequila.png",
+  "html/img/items/whisky.png",
+  "html/img/items/limonade.png",
+  "html/img/items/phone.png",
+  "html/img/items/vodka.png",
+  "html/img/items/water.png",
+  "html/img/items/cupcake.png",
+  "html/img/items/drpepper.png",
+  "html/img/items/energy.png",
+  "html/img/items/croquettes.png",
+  "html/img/items/bolpistache.png",
+  "html/img/items/bolnoixcajou.png",
+  "html/img/items/bolcacahuetes.png",
+  "html/img/items/fixkit.png",
+  "html/img/items/bolchips.png",
+  "html/img/items/black_chip.png",
+  "html/img/items/black_money.png",
+  "html/img/items/gym_membership.png",
+  "html/img/items/WEAPON_APPISTOL.png",
+  "html/img/items/WEAPON_ASSAULTRIFLE.png",
+  "html/img/items/WEAPON_ASSAULTSHOTGUN.png",
+  "html/img/items/WEAPON_BOTTLE.png",
+  "html/img/items/WEAPON_BULLPUPRIFLE.png",
+  "html/img/items/WEAPON_CARBINERIFLE.png",
+  "html/img/items/WEAPON_COMBATMG.png",
+  "html/img/items/WEAPON_BAT.png",
+  "html/img/items/WEAPON_COMBATPISTOL.png",
+  "html/img/items/WEAPON_CROWBAR.png",
+  "html/img/items/WEAPON_GOLFCLUB.png",
+  "html/img/items/WEAPON_KNIFE.png",
+  "html/img/items/WEAPON_MICROSMG.png",
+  "html/img/items/WEAPON_NIGHTSTICK.png",
+  "html/img/items/WEAPON_HAMMER.png",
+  "html/img/items/WEAPON_PISTOL.png",
+  "html/img/items/WEAPON_PUMPSHOTGUN.png",
+  "html/img/items/WEAPON_SAWNOFFSHOTGUN.png",
+  "html/img/items/WEAPON_SMG.png",
+  "html/img/items/WEAPON_STUNGUN.png",
+  "html/img/items/WEAPON_SPECIALCARBINE.png",
+  "html/img/items/WEAPON_KNUCKLE.png",
+  "html/img/items/WEAPON_FLASHLIGHT.png",
+  "html/img/items/WEAPON_REVOLVER.png",
+  "html/img/items/WEAPON_DAGGER.png",
+  "html/img/items/WEAPON_PETROLCAN.png",
+  "html/img/items/WEAPON_PISTOL50.png",
+  "html/img/items/WEAPON_DBSHOTGUN.png",
+  "html/img/items/WEAPON_SWITCHBLADE.png",
+  "html/img/items/WEAPON_POOLCUE.png"
 }
