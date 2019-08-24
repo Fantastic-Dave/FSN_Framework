78d9319c4ecca0c2b8e59821ec3c3c03b53d84eb
JamesSc0tt <jamesscott.js95@gmail.com>
2019-08-24 18:54:02 +0100

    :fsn_inventory: new preset items

---
+++ resources/[fsn]/fsn_inventory/cl_presets.lua
@@ -0,0 +1,60 @@
+presetItems = {
+	["bandage"] = {
+		index = 'bandage',
+		name = 'Bandage',
+		data = {
+			weight = 0.4
+		},
+	},
+}
+
+--[[
+local firstInventory = {
+	{
+		index='evidence',
+		name='Unsealed Evidence',
+		amt=1,
+		data = {
+			weight = 1,
+		},
+		customData = {
+			location = 'Elgin Ave',
+			call = '10-13',
+			notes = 'Shots fired',
+			contents = '[1X] 9mm casing (S:147y1721)'
+		}
+	},
+	{
+		index="id",
+		name='ID Card (Logan Whitehead)',
+		amt=1,
+		data = {
+		
+		},
+		customData = {
+			Name = 'Logan Whitehead',
+			Occupation = 'Police Officer',
+			DOB = '14/06/1998',
+			Issue = '18/08/2019'
+		},
+	},
+	{
+		index = 'bandage',
+		name = 'Bandage',
+		amt = 4
+	},
+	{
+		index = 'sandwich',
+		name = 'Sandwich',
+		amt = 2,
+		data = {
+			ass = 'i am a sandwich baby'
+		}
+	},
+	{
+		index = 'bandage',
+		name = 'Bandage',
+		amt = 4
+	},
+}
+]]--
