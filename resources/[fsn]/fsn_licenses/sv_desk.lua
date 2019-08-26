be5cea878123d0b16a23214dad0028b2a215679f
JamesSc0tt <jamesscott.js95@gmail.com>
2019-08-26 15:37:30 +0100

    :fsn_licenses: new ID card collection @ city hall

---
+++ resources/[fsn]/fsn_licenses/sv_desk.lua
@@ -0,0 +1,24 @@
+RegisterServerEvent('fsn_licenses:id:request')
+AddEventHandler('fsn_licenses:id:request', function(tbl)
+	local occu = 'Unemployed'
+	if tbl.char_police > 0 then
+		occu = 'Police'
+	end
+	if tbl.char_ems > 0 then
+		occu = 'EMS'
+	end
+	TriggerClientEvent('fsn_inventory:items:add', source, {
+		index="id",
+		name='ID Card ('..tbl.char_fname..' '..tbl.char_lname..')',
+		amt=1,
+		data = {
+		
+		},
+		customData = {
+			Name = tbl.char_fname..' '..tbl.char_lname,
+			Occupation = occu,
+			DOB = tbl.char_dob,
+			Issue = os.date("%x"),
+		},
+	})
+end)
