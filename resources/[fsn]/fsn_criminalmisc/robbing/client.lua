f3a8c1c7d634f86a890e3a42c0966b84c6220b9d
JamesSc0tt <jamesscott.js95@gmail.com>
2019-09-08 13:15:42 +0100

    :fsn_criminalmisc: new robbing system

--- resources/[fsn]/fsn_criminalmisc/robbing/client.lua
+++ resources/[fsn]/fsn_criminalmisc/robbing/client.lua
@@ -1,43 +1,15 @@
-local lastRob = 0
-local lastAttempt = 0
-local beingRobbed = false
-
-RegisterNetEvent('fsn_criminalmisc:robbing:startRob')
-AddEventHandler('fsn_criminalmisc:robbing:startRob', function(srv_id)
-	print'iambeingrobbed'
-	local maff = lastAttempt + 3
-	if maff < myTime or lastAttempt == 0 then
-		local newMaff = lastRob + 600
-		if maff < myTime or lastAttempt == 0 then
-			if not beingRobbed then
-				TriggerServerEvent('fsn_criminalmisc:robbing:startRobbing', srv_id)
-				local dm = 0
-				if exports['fsn_inventory']:fsn_HasItem('dirty_money') > 0 then
-					dm = exports['fsn_inventory']:fsn_GetItemAmount('dirty_money')
-				end
-				local money = 0
-				if exports["fsn_main"]:fsn_GetWallet() > 0 then
-					money = exports["fsn_main"]:fsn_GetWallet()		
-				end
-				Citizen.Wait(8000)
-				TriggerServerEvent('fsn_criminalmisc:robbing:finishRob', srv_id, dm, money)
-				beingRobbed = false
-				lastRob = myTime
-			else
-				TriggerServerEvent('fsn_notify:displayNotification', srv_id, 'Player is being robbed right now!.', 'centerLeft', 4000, 'error')
-			end
-		else
-			TriggerServerEvent('fsn_notify:displayNotification', srv_id, 'Player has been robbed too recently.', 'centerLeft', 4000, 'error')
-		end
-	end
-end)
-
 RegisterNetEvent('fsn_criminalmisc:robbing:startRobbing')
-AddEventHandler('fsn_criminalmisc:robbing:startRobbing', function()
+AddEventHandler('fsn_criminalmisc:robbing:startRobbing', function(ply)
 	while not HasAnimDictLoaded('mini@triathlon') do
 		RequestAnimDict('mini@triathlon')
 		Citizen.Wait(5)
 	end
 	TaskPlayAnim(GetPlayerPed(-1), "mini@triathlon", "rummage_bag", 8.0, -8, -1, 48, 0, 0, 0, 0)
-	Citizen.Wait(2500)	
+	Citizen.Wait(2500)
+	if GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(ply)), GetEntityCoords(GetPlayerPed(-1)), true) < 3 then
+		exports['mythic_notify']:DoHudText('success', 'Robbing player: '..GetPlayerServerId(ply))
+		TriggerServerEvent('fsn_inventory:sys:request', GetPlayerServerId(ply), GetPlayerServerId(PlayerId()))
+	else
+		exports['mythic_notify']:DoHudText('error', 'Player is too far away!')
+	end
 end)
