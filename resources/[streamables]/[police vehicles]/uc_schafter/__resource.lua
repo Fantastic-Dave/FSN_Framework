f85af5ce0f20644a1f1d4c9a995c054bf219e6b7
SilverNick <32308171+SilverNick@users.noreply.github.com>
2019-09-08 14:07:34 -0400

    :streamables: PD car fix

--- resources/[streamables]/[police vehicles]/uc_schafter/__resource.lua
+++ resources/[streamables]/[police vehicles]/uc_schafter/__resource.lua
@@ -4,9 +4,11 @@ files {
 	'vehicles.meta',
 	'carvariations.meta',
 	'carcols.meta',
+	'data/handling.meta',
 
 }
 
+data_file 'HANDLING_FILE' 'data/handling.meta'
 data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
 data_file 'CARCOLS_FILE' 'carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
