00f8508d8ea638c5a748e2598e811979eddc615f
SilverNick <32308171+SilverNick@users.noreply.github.com>
2019-09-11 23:46:05 -0400

    :fsn_builders: Added more fields to data

--- resources/[fsn]/fsn_builders/schemas/ccarhandlingdata.lua
+++ resources/[fsn]/fsn_builders/schemas/ccarhandlingdata.lua
@@ -6,3 +6,21 @@ Field "float" "fBackEndPopUpBuildingImpulseMult"
 
 Field "float" "fBackEndPopUpMaxDeltaSpeed"
 	Default (0.6)
+
+Field "float" "fCamberFront"
+	Default (0.0)
+
+Field "float" "fCamberRear"
+	Default (0.0)
+
+Field "float" "fCastor"
+	Default (0.0)
+
+Field "float" "fToeFront"
+	Default (0.0)
+
+Field "float" "fToeRear"
+	Default (0.0)
+
+Field "float" "fEngineResistance"
+	Default (0.0)
