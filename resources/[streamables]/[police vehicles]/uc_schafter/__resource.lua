4ea09e7c3abe5832a53249a523c02d7eec107a66
SilverNick <32308171+SilverNick@users.noreply.github.com>
2019-09-20 19:00:47 -0400

    :streamables: Schafter Fix tell me next time

--- resources/[streamables]/[police vehicles]/uc_schafter/__resource.lua
+++ resources/[streamables]/[police vehicles]/uc_schafter/__resource.lua
@@ -4,11 +4,11 @@ files {
 	'vehicles.meta',
 	'carvariations.meta',
 	'carcols.meta',
-	'data/handling.meta',
+	'handling.meta',
 
 }
 
-data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'HANDLING_FILE' 'handling.meta'
 data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
 data_file 'CARCOLS_FILE' 'carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
