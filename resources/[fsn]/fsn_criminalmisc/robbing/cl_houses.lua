b03502a1e0b61289d71e4adacdb460485ea06560
James Lee Scott <me@jamesscott.pw>
2019-09-26 13:00:21 +0100

    :fsn_criminalmisc: remove blips for houses

--- resources/[fsn]/fsn_criminalmisc/robbing/cl_houses.lua
+++ resources/[fsn]/fsn_criminalmisc/robbing/cl_houses.lua
@@ -1,13 +1,13 @@
 Citizen.CreateThread(function()
 	--while true do Citizen.Wait(0)
 		for k, v in pairs(robbables) do
-			local bleep = AddBlipForCoord(v.x, v.y, v.z)
-			SetBlipSprite(bleep, 1)
-			SetBlipColour(bleep, 3)
-			SetBlipAsShortRange(bleep, true)
-			BeginTextCommandSetBlipName("STRING")
-			AddTextComponentString("Robbable Property")
-			EndTextCommandSetBlipName(bleep)
+			--local bleep = AddBlipForCoord(v.x, v.y, v.z)
+			--SetBlipSprite(bleep, 1)
+			--SetBlipColour(bleep, 3)
+			--SetBlipAsShortRange(bleep, true)
+			--BeginTextCommandSetBlipName("STRING")
+			--AddTextComponentString("Robbable Property")
+			--EndTextCommandSetBlipName(bleep)
 		end
 	--end
 end)
