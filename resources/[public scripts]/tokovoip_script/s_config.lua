9e51f7f7657f5eeb1df1a375a0e3eca3da311ee5
SilverNick <32308171+SilverNick@users.noreply.github.com>
2019-09-28 00:33:58 -0400

    :Toko: Updated radio name

--- resources/[public scripts]/tokovoip_script/s_config.lua
+++ resources/[public scripts]/tokovoip_script/s_config.lua
@@ -2,7 +2,7 @@ TokoVoipConfig = {
 	channels = {
 		{name = "LSPD Radio", subscribers = {}},
 		{name = "LEO Shared Radio", subscribers = {}},
-		{name = "BCSO Radio", subscribers = {}},
+		{name = "SASP Radio", subscribers = {}},
 		--{name = "LEO/EMS Shared Radio", subscribers = {}},
 		{name = "EMS Radio", subscribers = {}},
 	}
