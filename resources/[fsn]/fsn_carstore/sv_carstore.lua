1b94c3bf2c132b8dc953a88a51ebe14f7f1dc8d6
SilverNick <32308171+SilverNick@users.noreply.github.com>
2019-10-18 19:11:56 -0400

    :fsn_carstore: Reaper price bump

--- resources/[fsn]/fsn_carstore/sv_carstore.lua
+++ resources/[fsn]/fsn_carstore/sv_carstore.lua
@@ -112,7 +112,7 @@ local vehicles = {
 		{name = "Osiris", costs = 7000000, description = {}, model = "osiris"},
 		{name = "Pariah", costs = 750000, description = {}, model = "pariah"},
 		{name = "Pfister 811", costs = 750000, description = {}, model = "pfister811"},
-		{name = "Reaper", costs = 600000, description = {}, model = "reaper"},
+		{name = "Reaper", costs = 1100000, description = {}, model = "reaper"},
 		{name = "SC1", costs = 2500000, description = {}, model = "sc1"},
 		{name = "Sultan RS", costs = 325000, description = {}, model = "sultanrs"},
 		{name = "T20", costs = 6700000, description = {}, model = "t20"},
